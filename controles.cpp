38256d993e87a1c4b4cbb53e8351a061c6a07a30
Santiago <santiago.taborda2@udea.edu.co>
2018-12-03 12:17:07 -0500

    clase usuario

---
+++ controles.cpp
@@ -0,0 +1,6 @@
+#include "controles.h"
+
+controles::controles()
+{
+
+}
---
+++ controles.h
@@ -0,0 +1,11 @@
+#ifndef CONTROLES_H
+#define CONTROLES_H
+
+
+class controles
+{
+public:
+    controles();
+};
+
+#endif // CONTROLES_H
