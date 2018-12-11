187bf584a995cc7eabad188abc5a1717f7f2ed32
Santiago <santiago.taborda2@udea.edu.co>
2018-12-11 00:13:02 -0500

    screensplash

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include "registro.h"
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSplashScreen>
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QSplashScreen * splash= new QSplashScreen;
+    splash->setPixmap(QPixmap(":/images/spacesplash.png"));
+    splash->show();
     registro w;
-    w.show();
+
+    QTimer::singleShot(2500,splash,SLOT(close()));
+    QTimer::singleShot(2500,&w,SLOT(show()));
+
 
     return a.exec();
 }
